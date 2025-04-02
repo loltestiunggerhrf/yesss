4ce8d809c26b47f08a0031d5e806875110757631
loltestiunggerhrf <nativeroleplayrblx@gmail.com>
2025-04-02 11:08:04 +0700

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,8 +7,6 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
-def run():
-    app.run(host="0.0.0.0", port=8080)
-
 if __name__ == "__main__":
-    run()
+    port = int(os.getenv("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
